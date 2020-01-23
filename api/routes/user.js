f087f5f926669b38811d2f7a20a4f6c3e5d8c0c3
DankoMaksym <dankomaksym@gmail.com>
2020-01-24 00:30:06 +0200

    Adding User Login & JWT Signing

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -46,6 +47,52 @@ router.post('/signup', (req, res, next) => {
     })
     .catch();
   
+})
+
+
+router.post('/login', (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        })
+      } else {
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (err) {
+            return result.status(401).json({
+              message: 'Auth failed'
+            });
+          }
+          if (result) {
+            const token = jwt.sign(
+              {
+                email: user[0].email,
+                userId: user[0]._id
+              }, 
+              process.env.JWT_KEY,
+              {
+                expiresIn: '1h'
+              }
+            );
+            return res.status(200).json({
+              message: 'Auth successful',
+              token: token
+            });
+          }
+          res.status(401).json({
+            message: 'Auth failed'
+          })
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
 
